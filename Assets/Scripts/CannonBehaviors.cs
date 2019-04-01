41a5b7f380cae61900d32984aa15eb0a463694fc
Austin Meyer <meyerforge@gmail.com>
2019-04-01 11:29:15 -0700

    Finished basic cannon

--- Assets/Scripts/CannonBehaviors.cs
+++ Assets/Scripts/CannonBehaviors.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CannonBehaviors : MonoBehaviour
 {
     public GameObject foodPref;
+    public Vector2 pointOfRotation;
+    public float raidusFromPoint;
 
 
     public float SpeedCommand;
@@ -14,13 +16,17 @@ public class CannonBehaviors : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        float x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - this.transform.position.x;
+        float y = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - this.transform.position.y;
+        float angleCommand = Mathf.Rad2Deg * Mathf.Atan2(y, x);
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             var food = Instantiate(foodPref, this.transform.position, Quaternion.identity);
             food.GetComponent<FoodBehaviors>().SetInitVelocity(SpeedCommand, angleCommand);
