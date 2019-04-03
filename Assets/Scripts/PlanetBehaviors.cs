e41e3ee682aa8efc1d841fe6f8da5963aa30301f
Austin Meyer <meyerforge@gmail.com>
2019-04-03 14:37:02 -0700

    Merge broke Unity specific items - Hopefully this will help

--- Assets/Scripts/PlanetBehaviors.cs
+++ Assets/Scripts/PlanetBehaviors.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlanetBehaviors : MonoBehaviour
 {
+    private const float GRAV_CONST = 0.50f;
+
+
     public float mass;
+    public float cookingRadius;
 
 
-    private float graitationalConst;
+
     private Rigidbody2D rig;
     private GameObject[] food;
 
@@ -15,42 +19,64 @@ public class PlanetBehaviors : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        graitationalConst = 0.25f;
         food = GameObject.FindGameObjectsWithTag("Food");
         rig = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        GravityUpdate();
+        food = GameObject.FindGameObjectsWithTag("Food");
+        AffectFood();
     }
 
 
-    private void GravityUpdate()
+    private void AffectFood()
     {
-        float distance = 0;
+        foreach (GameObject pieceOf in food)
+        {
+            GravityUpdate(pieceOf);
+            CookFood(pieceOf);
+        }
+    }
+
+
+    private void GravityUpdate(GameObject pieceOf)
+    {
         float force = 0;
         food = GameObject.FindGameObjectsWithTag("Food");
 
-        if (food.Length > 0)
+
+        // Find F = (mm)/d^2
+        force = ((pieceOf.GetComponent<Rigidbody2D>().mass * mass) / CalculateDistance(pieceOf)) * GRAV_CONST;
+
+        Vector2 newVelocity = new Vector2(force * Mathf.Cos(CalculateAngle(pieceOf)), force * Mathf.Sin(CalculateAngle(pieceOf)));
+
+        pieceOf.GetComponent<FoodBehaviors>().AddVelocity(newVelocity);
+    }
+
+
+    private void CookFood(GameObject pieceOf)
+    {
+        if (cookingRadius >= CalculateDistance(pieceOf))
         {
-            foreach (GameObject pieceOf in food)
-            {
-                // Find F = (mm)/d^2
-                distance = Mathf.Pow(Vector2.Distance(pieceOf.transform.position, this.transform.position), 2);
-                force = ((pieceOf.GetComponent<Rigidbody2D>().mass * mass) / distance) * graitationalConst;
-
-
-                // Find the direction of new force
-                float x = (this.transform.position.x - pieceOf.transform.position.x);
-                float y = (this.transform.position.y - pieceOf.transform.position.y);
-                float angle = Mathf.Atan2(y, x);
-
-                Vector2 newVelocity = new Vector2(force * Mathf.Cos(angle), force * Mathf.Sin(angle));
-
-                pieceOf.GetComponent<FoodBehaviors>().AddVelocity(newVelocity);
-            }
+            pieceOf.GetComponent<FoodBehaviors>().CookTimer();
         }
+    }
+
+
+    private float CalculateDistance(GameObject pieceOf)
+    {
+        return Mathf.Pow(Vector2.Distance(pieceOf.transform.position, this.transform.position), 2);
+    }
+
+
+    private float CalculateAngle(GameObject pieceOf)
+    {
+        // Find the direction of new force
+        float x = (this.transform.position.x - pieceOf.transform.position.x);
+        float y = (this.transform.position.y - pieceOf.transform.position.y);
+
+        return Mathf.Atan2(y, x);
     }
 }
